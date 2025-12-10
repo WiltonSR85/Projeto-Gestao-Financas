874ab3839b76ba9f33ad24fb0221825d84a9e9d4
Luisa Mel A. M. <luisamelstudies@gmail.com>
2025-12-10 06:24:35 -0300

    Adição de category_data

---
+++ code/lib/models/category_data.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class CategoryData {
+  final IconData icon;
+  final String title;
+  final Color color;
+
+  const CategoryData({
+    required this.icon,
+    required this.title,
+    required this.color,
+  });
+}
